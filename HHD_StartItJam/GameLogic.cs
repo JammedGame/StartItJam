e1e6833f9d016227c66c2e5638e5360a2938797a
Unknown <manojlovicsmilos@hotmail.com>
2017-07-14 22:50:41 +0200

    Project init

---
+++ HHD_StartItJam/GameLogic.cs
@@ -0,0 +1,41 @@
+﻿using Engineer.Engine;
+using Engineer.Runner;
+using OpenTK.Graphics;
+using Engineer.Draw;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engineer.Engine.IO;
+
+namespace HHD_StartItJam
+{
+    public class GameLogic
+    {
+        private EFXInterface _EFX;
+        private ExternRunner _Runner;
+        private Game _Game;
+        private Scene _Menu;
+        private Scene _Current;
+        public GameLogic()
+        {
+            ResourceManager _ResMan = new ResourceManager();
+            _ResMan.Init();
+            this._EFX = new EFXInterface();
+            this._Game = new Game();
+            this._Runner = new ExternRunner(1024, 600, new GraphicsMode(32, 24, 0, 8), "Death de sombre!");
+            this._Runner.WindowState = OpenTK.WindowState.Normal;
+            Engineer.Engine.Settings.GraphicsQuality = Quality.High;
+            this._Current = new Scene2D("Test");
+            this._Game.AddScene(this._Current);
+            DrawnSceneObject Dragon = (DrawnSceneObject)this._EFX.Load("Data/Dragon.efx");
+            this._Current.AddSceneObject(Dragon);
+        }
+        public void Run()
+        {
+            this._Runner.Init(this._Game, this._Current);
+            this._Runner.Run();
+        }
+    }
+}
---
+++ HHD_StartItJam/ResourceManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace HHD_StartItJam
+{
+    public class ResourceManager
+    {
+        public static Dictionary<string, Bitmap> Images;
+        public static ResourceManager RM;
+        public ResourceManager()
+        {
+            XmlDocument Document = new XmlDocument();
+            Document.Load("Data/Default.mtx");
+            XmlNode Main = Document.FirstChild;
+            Engineer.Engine.Material Mat = new Engineer.Engine.Material(Main);
+            Engineer.Engine.Material.Default = Mat;
+        }
+        public void Init()
+        {
+            RM = this;
+            Images = new Dictionary<string, Bitmap>();
+            ResourceSet resourceSet = global::HHD_StartItJam.Properties.Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+            foreach (DictionaryEntry entry in resourceSet)
+            {
+                string resourceKey = entry.Key.ToString();
+                if (resourceKey.StartsWith("audio_")) continue;
+                object resource = entry.Value;
+                Images.Add(resourceKey, (Bitmap)resource);
+            }
+        }
+
+    }
+}

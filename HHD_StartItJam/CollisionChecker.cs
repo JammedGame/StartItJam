0e1ffa9ae566cd3c6afd5994e607108a6a974225
Unknown <manojlovicsmilos@hotmail.com>
2017-07-15 02:50:42 +0200

    Collision Checker

---
+++ HHD_StartItJam/CollisionChecker.cs
@@ -0,0 +1,27 @@
+﻿using Engineer.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHD_StartItJam
+{
+    public class CollisionChecker
+    {
+        public static SceneObject CheckSceneCollision(Scene CurrentScene, SceneObject CurrentObject)
+        {
+            for(int i = 0; i < CurrentScene.Objects.Count; i++)
+            {
+                if(CurrentScene.Objects[i].Data.ContainsKey("Collision") && (bool)CurrentScene.Objects[i].Data["Collision"] == true)
+                {
+                    if (((Sprite)CurrentObject.Visual).InCollision(CurrentScene.Objects[i].Visual, Engineer.Mathematics.Collision2DType.Rectangular))
+                    {
+                        return CurrentScene.Objects[i];
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
